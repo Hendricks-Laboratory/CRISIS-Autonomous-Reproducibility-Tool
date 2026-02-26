ba98be6dd85b07304da5497908d7f7e4f9d4aa61
Audrey Marthin <audreymarthin22@gmail.com>
2026-02-26 03:03:23 -0800

    Fixed typo

--- web/app.py
+++ web/app.py
@@ -6,8 +6,8 @@ from itertools import combinations
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use("Agg")
-from helpers import group
-from detect import detectOutliers, controlSensitizingGraph
+from .helpers import group
+from .detect import detectOutliers, controlSensitizingGraph
 from gpr.model import run_gp_pipeline, apply_feature_engineering, auto_detect_features
 from gpr.visualize import plot_1d, plot_2d, plot_3d
 
